8d7f2304fe3fb3f97a4528d88bdfa8ed5449fff1
lzb <465222498@qq.com>
2017-04-05 14:59:11 +0800

    资源文件

---
+++ src/commom/js/util.js
@@ -0,0 +1,28 @@
+/**
+ * 解析url参数
+ * @example ?id=123&a=b
+ * @return Object {id:1234,a:b}
+ */
+export function urlParse() {
+  let url = window.location.search;
+  let obj = {};
+  let reg = /[?&][^?&]+=[^?&]+/g;
+  let arr = url.match(reg);
+  // ['?id=123','&a=b']
+  if (arr) {
+    arr.forEach((item) => {
+      let tempArr = item.substr(1).split('=');
+      let key = decodeURIComponent(tempArr[0]);
+      let val = decodeURIComponent(tempArr[1]);
+      obj[key] = val;
+    });
+  }
+  return obj;
+}
+
+/*
+function GetQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return (decodeURI(r[2]));
+  return '';*/
